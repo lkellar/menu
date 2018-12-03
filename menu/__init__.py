480813e0806cf2a058300401d68fd281f935eb16
Lucas Kellar <katzrkool@protonmail.com>
2018-12-03 10:07:36 -0600

    added reset cache and stopped opening a file so many times

--- menu/__init__.py
+++ menu/__init__.py
@@ -1,1 +1,12 @@
 from menu.app import app
+import os, json
+from menu.fetcher import Fetcher
+
+def resetCache():
+    currentDir = os.path.dirname(os.path.realpath(__file__))
+    configPath = os.path.join(currentDir, '../config.json')
+    with open(configPath, 'r') as f:
+        config = json.load(f)
+    config['cache'] = config['cache'].replace('$HERE', os.path.join(currentDir, '..'))
+    fetch = Fetcher(config)
+    fetch.resetCache()
--- menu/fetcher.py
+++ menu/fetcher.py
@@ -12,21 +12,27 @@ class Fetcher:
 
     def week(self, monday: datetime):
         data = {}
+        cache = self.loadCache()
         for i in range(0, 5):
             date = monday + timedelta(days=i)
-            menuData = self.get(False, date)
+            menuData = self.get(False, date, cache=cache)
             if len(menuData[date.strftime('%Y-%m-%d')]) > 0:
                 data.update(menuData)
         return data
 
-    def get(self, prettify: bool, date: datetime):
+    def loadCache(self) -> dict:
+        with io.open(self.cache, "r", encoding='utf8') as f:
+            try:
+                return json.load(f)
+            except json.decoder.JSONDecodeError:
+                data = self.scrape()
+                self.save(data)
+                return data
+
+    def get(self, prettify: bool, date: datetime, cache: dict = None):
         if isfile(self.cache):
-            with io.open(self.cache, "r", encoding='utf8') as f:
-                try:
-                    cache = json.load(f)
-                except json.decoder.JSONDecodeError:
-                    self.save(self.scrape())
-                    return self.get(prettify, date)
+            if not cache:
+                cache = self.loadCache()
 
             isoDate = date.strftime('%Y-%m-%d')
             currentMonth = datetime.today().month
@@ -52,6 +58,9 @@ class Fetcher:
 
     def scrape(self, months: int = 0):
         return Scraper(self.school, self.menu, months).go()
+
+    def resetCache(self):
+        self.save(self.scrape())
 
     def save(self, data):
         with io.open(self.cache, 'w', encoding='utf8') as f:
