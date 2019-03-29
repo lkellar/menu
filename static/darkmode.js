8672e0415ffdb2d6716aea12845eef8bcc5c6651
Lucas K <lucas@lkellar.org>
2019-03-28 21:20:40 -0500

    updated darkmode to respect user preferences better

--- static/darkmode.js
+++ static/darkmode.js
@@ -1,15 +1,16 @@
-function triggerDarkMode(on) {
-    if (on) {
-        localStorage.setItem('darkmode', 'true');
-    } else {
-        localStorage.setItem('darkmode', 'false');
+function triggerDarkMode(on, set=true) {
+    if (set) {
+        localStorage.setItem('darkmode',  on.toString())
     }
-    document.body.classList.toggle('darkmode');
+    if (document.body.classList.contains('darkmode')) {
+        document.body.classList.remove('darkmode');
+    } else {
+        document.body.classList.add('darkmode');
+    }
 }
 
 function toggle() {
-    const darkValue = localStorage.getItem('darkmode');
-    if (darkValue === 'true') {
+    if (document.body.classList.contains('darkmode')) {
       triggerDarkMode(false);
     } else {
         triggerDarkMode(true);
@@ -17,19 +18,35 @@ function toggle() {
 }
 
 function darkTest(e) {
-    if (e.matches) {
-        triggerDarkMode(true);
-    } else {
-        triggerDarkMode(false);
+    // If the user already has a pref set, we don't want to adjust the color based on system pref
+    if (!localStorage.getItem('darkmode')) {
+        if (e.matches) {
+            triggerDarkMode(true, false);
+        } else {
+            triggerDarkMode(false, false);
+        }
     }
 }
 
 function init() {
-    const darkValue = localStorage.getItem('darkmode');
+    const darkPref = localStorage.getItem('darkmode');
     const mql = window.matchMedia('(prefers-color-scheme: dark)');
     mql.addListener(darkTest);
-    if (darkValue === 'true' || mql.matches) {
-        triggerDarkMode(true);
+
+    // Some of the things in the statements below are redundant, but I really want to make sure someone's preference
+    // doesn't get messed up.
+    if (darkPref) {
+        if (darkPref === 'true') {
+            triggerDarkMode(true);
+        } else if (darkPref === 'false') {
+            triggerDarkMode(false);
+        }
+    } else {
+        if (mql.matches) {
+            triggerDarkMode(true, false);
+        } else {
+            triggerDarkMode(false, false);
+        }
     }
 }
 
